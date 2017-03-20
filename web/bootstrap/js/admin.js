25e0d8661ddc347ddb64c60e31c50912cb6cc640
yeyangzi <1501421196@qq.com>
2017-03-20 17:08:38 +0800

    管理页面按钮页面，三个分页面，都实现删除功能

--- web/bootstrap/js/admin.js
+++ web/bootstrap/js/admin.js
@@ -2,21 +2,6 @@
  * Created by yey on 2017/3/19.
  */
 $(function(){
-    /*footer始终在底部*/
-    pageButtomPos();
-    function pageButtomPos(){
-        var min_height=600;
-        var screen_Height=window.innerHeight;
-        /*console.log(screen_Height)*/
-        var buttom=document.getElementsByClassName("footer")[0];
-        if(screen_Height<min_height){
-            buttom.style.top=(min_height-80)+"px";
-        }else{
-            buttom.style.top=(screen_Height-80)+"px";
-        }
-    }
-    $(window).resize(pageButtomPos);
-
     /*点击按钮跳转各自的后台管理界面*/
    /* $("#house_manegee").click(go_in_admin("house_manegee.html"));
     $("#area_manegee").click(go_in_admin("area_manegee.html"));
@@ -25,13 +10,13 @@ $(function(){
         console.log(b)
       /!*  window.location.href=b;*!/
     }*/
-    $("#house_manegee").click(function(){
-        window.location.href="#";
+    $("#house_manage").click(function(){
+        window.location.href="house_manage.html";
     })
-    $("#area_manegee").click(function(){
-        window.location.href="#";
+    $("#area_manage").click(function(){
+        window.location.href="area_manage.html";
     })
-    $("#user_manegee").click(function(){
-        window.location.href="#";
+    $("#user_manage").click(function(){
+        window.location.href="user_manage.html";
     })
 })
---
+++ web/bootstrap/js/admin_manage.js
@@ -0,0 +1,12 @@
+/**
+ * Created by yey on 2017/3/20.
+ */
+$(function(){
+    /*go-top¿ìËÙ»Ø¶¥*/
+    $(".go-top").click(function(){
+        $(window).scrollTop(0);
+    });
+    $(".house_news_del").click(function(){
+        $(this).parent().parent().css("display","none")
+    })
+})
