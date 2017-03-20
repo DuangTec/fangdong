6091f05e7572d7a3938cc72aa56709c1e2cbdfd1
yeyangzi <1501421196@qq.com>
2017-03-20 18:45:14 +0800

    三个后台编辑界面结构及样式

---
+++ web/bootstrap/js/admin_edit.js
@@ -0,0 +1,9 @@
+/**
+ * Created by yey on 2017/3/20.
+ */
+$(function(){
+    /*go-top¿ìËÙ»Ø¶¥*/
+    $(".go-top").click(function(){
+        $(window).scrollTop(0);
+    });
+})
--- web/bootstrap/js/admin_manage.js
+++ web/bootstrap/js/admin_manage.js
@@ -10,12 +10,12 @@ $(function(){
         $(this).parent().parent().css("display","none");
     })
     $(".house_manage .house_news_edit").click(function(){
-        window.location.href='house_manage_udit.html';
+        window.location.href='house_manage_edit.html';
     })
     $(".area_manage .house_news_edit").click(function(){
-        window.location.href='area_manage_udit.html';
+        window.location.href='area_manage_edit.html';
     })
     $(".user_manage .house_news_edit").click(function(){
-        window.location.href='user_manage_udit.html';
+        window.location.href='user_manage_edit.html';
     })
 })
