a1259d5e8c322343f21a5ffbf8c889a40b2825da
yeyangzi <1501421196@qq.com>
2017-03-20 17:34:54 +0800

    三个管理界面实现跳转到各自的编辑界面

--- web/bootstrap/js/admin_manage.js
+++ web/bootstrap/js/admin_manage.js
@@ -7,6 +7,15 @@ $(function(){
         $(window).scrollTop(0);
     });
     $(".house_news_del").click(function(){
-        $(this).parent().parent().css("display","none")
+        $(this).parent().parent().css("display","none");
+    })
+    $(".house_manage .house_news_edit").click(function(){
+        window.location.href='house_manage_udit.html';
+    })
+    $(".area_manage .house_news_edit").click(function(){
+        window.location.href='area_manage_udit.html';
+    })
+    $(".user_manage .house_news_edit").click(function(){
+        window.location.href='user_manage_udit.html';
     })
 })
