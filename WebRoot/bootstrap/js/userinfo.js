cc3ce2435d70581ae3624ed1ea0c485b810730e3
“SouthWind <516345614@qq.com>
2017-03-22 23:13:08 +0800

    增加用户信息

---
+++ WebRoot/bootstrap/js/userinfo.js
@@ -0,0 +1,13 @@
+/**
+ * Created by sony on 2017/3/22.
+ */
+//房屋删除
+function delHome(id){
+    window.location.href=" "+id;
+
+}
+
+// 房屋编辑
+function editHome(id){
+    window.location.href=" "+id;
+}
