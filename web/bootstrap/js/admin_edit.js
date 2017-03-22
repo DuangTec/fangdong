341ecef076a8e5b4406d2219c877b844505d9f7c
yeyangzi <1501421196@qq.com>
2017-03-22 13:00:25 +0800

    后台编辑模拟从后台拿数据

--- web/bootstrap/js/admin_edit.js
+++ web/bootstrap/js/admin_edit.js
@@ -11,11 +11,17 @@ $(function(){
         $(".edit_select_icon").css("display","block");
     })
     $(".edit_select_icon").click(function(){
+        $(".edit-select_ul").append("<li id='top1'>fff</li>")
         $(".edit-select_ul").slideToggle(500);
+        $(".edit-select_ul li").click(function(){
+            $(".edit_select input").attr("value",$(this).html());
+            /*alert($(".edit_select input").attr("value"));
+             alert($(".edit_select input").val());*/
+            $(".edit_select input").attr("id",$(this).attr("id"));
+            $(".select_control_label").attr("for",$(this).attr("id"));
+            $(".edit-select_ul").slideUp(500);
+            $(".edit_select_icon").css("display","none");
+        })
     })
-    $(".edit-select_ul li").click(function(){
-        $(".edit_select input").val($(this).html());
-        $(".edit-select_ul").slideToggle(500);
-        $(".edit_select_icon").css("display","none");
-    })
+
 })
