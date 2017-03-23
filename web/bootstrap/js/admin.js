eb84bfb9aa429f52383a799a75413f53334c925d
yeyangzi <1501421196@qq.com>
2017-03-23 15:11:45 +0800

    footer宽度问题

--- web/bootstrap/js/admin.js
+++ web/bootstrap/js/admin.js
@@ -2,6 +2,10 @@
  * Created by yey on 2017/3/19.
  */
 $(function(){
+    /*go-top¿ìËÙ»Ø¶¥*/
+    $(".go-top").click(function(){
+        $(window).scrollTop(0);
+    });
     $("#house_manage").click(function(){
         window.location.href="house_manage.html";
     })
