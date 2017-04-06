375d27c5a9fbd54fda1344e268b54e9fd9806255
yeyangzi <1501421196@qq.com>
2017-04-06 19:39:59 +0800

    新增后台新增四个管理和编辑界面，还剩地铁编辑页面未写

--- web/bootstrap/js/admin_manage.js
+++ web/bootstrap/js/admin_manage.js
@@ -6,10 +6,15 @@ $(function(){
     $(".go-top").click(function(){
         $(window).scrollTop(0);
     });
-    /*删除编辑记录页面跳转*/
+    /*管理界面删除条目*/
     $(".house_news_del").click(function(){
         $(this).parent().parent().css("display","none");
     })
+    /*地铁线路删除*/
+    $(".subway_line_del").click(function(){
+        $(this).parents(".panel").css("display","none");
+    })
+    /*编辑跳转*/
     $(".house_manage .house_news_edit").click(function(){
         window.location.href='house_manage_edit.html';
     })
@@ -19,6 +24,13 @@ $(function(){
     $(".user_manage .house_news_edit").click(function(){
         window.location.href='user_manage_edit.html';
     })
+    $(".hot_area_manage .house_news_edit").click(function(){
+        window.location.href='hot_area_manage_edit.html';
+    })
+    $(".subway_manage .house_news_edit").click(function(){
+        window.location.href='subway_manage_edit.html';
+    })
+    /*添加条目跳转编辑*/
     $(".house_manage .add_news_but").click(function(){
         window.location.href='house_manage_edit.html';
     })
@@ -27,5 +39,14 @@ $(function(){
     })
     $(".user_manage .add_news_but").click(function(){
         window.location.href='user_manage_edit.html';
+    })
+    $(".hot_area_manage .add_news_but").click(function(){
+        window.location.href='hot_area_manage_edit.html';
+    })
+    $(".subway_manage .add_news_but").click(function(){
+        window.location.href='subway_manage_edit.html';
+    })
+    $(".subway_manage .subway_news_edit").click(function(){
+        window.location.href='subway_manage_edit.html';
     })
 })
