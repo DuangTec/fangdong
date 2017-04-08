6aa97548f0a26fabab9026e71bcb1af208f84914
yeyangzi <1501421196@qq.com>
2017-04-08 08:43:09 +0800

    管理编辑下拉多选js处理，输入框和下拉一一对应

--- web/bootstrap/js/admin_edit.js
+++ web/bootstrap/js/admin_edit.js
@@ -8,17 +8,17 @@ $(function(){
     });
     /*input聚焦下拉按钮出现，按钮点击模拟select出现，select点击input获得对应值*/
     $(".edit_select input").focus(function(){
-        $(".edit_select_icon").css("display","block");
+        $(this).parents(".edit_select").children(".edit_select_icon").css("display","block");
     })
     $(".edit_select_icon").click(function(){
-        $(".edit-select_ul").append("<li id='top1'>fff</li>")
-        $(".edit-select_ul").slideToggle(500);
+        $(this).parents(".edit_select").children(".edit-select_ul").append("<li id='top1'>fff</li>")
+        $(this).parents(".edit_select").children(".edit-select_ul").slideToggle(500);
         $(".edit-select_ul li").click(function(){
-            $(".edit_select input").attr("value",$(this).html());
+            $(this).parents(".edit_select").children("input").attr("value",$(this).html());
             /*alert($(".edit_select input").attr("value"));
              alert($(".edit_select input").val());*/
-            $(".edit_select input").attr("id",$(this).attr("id"));
-            $(".select_control_label").attr("for",$(this).attr("id"));
+            $(this).parents(".edit_select").children("input").attr("id",$(this).attr("id"));
+            $(this).parents(".form-group").children("label").attr("for",$(this).attr("id"));
             $(".edit-select_ul").slideUp(500);
             $(".edit_select_icon").css("display","none");
         })
