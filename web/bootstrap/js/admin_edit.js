2200281e66ba5e1f88621d57a78ebb5e22a7fad6
yeyangzi <1501421196@qq.com>
2017-04-08 10:35:09 +0800

    房屋管理增加审核跳转，地铁编辑结构完成还剩js

--- web/bootstrap/js/admin_edit.js
+++ web/bootstrap/js/admin_edit.js
@@ -11,7 +11,7 @@ $(function(){
         $(this).parents(".edit_select").children(".edit_select_icon").css("display","block");
     })
     $(".edit_select_icon").click(function(){
-        $(this).parents(".edit_select").children(".edit-select_ul").append("<li id='top1'>fff</li>")
+       /* $(this).parents(".edit_select").children(".edit-select_ul").append("<li id='top1'>fff</li>");*/
         $(this).parents(".edit_select").children(".edit-select_ul").slideToggle(500);
         $(".edit-select_ul li").click(function(){
             $(this).parents(".edit_select").children("input").attr("value",$(this).html());
@@ -19,8 +19,8 @@ $(function(){
              alert($(".edit_select input").val());*/
             $(this).parents(".edit_select").children("input").attr("id",$(this).attr("id"));
             $(this).parents(".form-group").children("label").attr("for",$(this).attr("id"));
-            $(".edit-select_ul").slideUp(500);
-            $(".edit_select_icon").css("display","none");
+            $(this).parents(".edit_select").children(".edit-select_ul").slideUp(500);
+            $(this).parents(".edit_select").children(".edit_select_icon").css("display","none");
         })
     })
     /*删除站点*/
@@ -31,18 +31,17 @@ $(function(){
         })
     }
     /*添加站点*/
-    $(".subway_station_add").focus(function(){
-        $(this).attr("value","");
-        $(this).val("");
+    $(".subway_station_add").click(function(){
+        $(".subway_station_add_select").slideToggle(100);
+        /*$(this).attr("value","");
+        $(this).val("");*/
     })
-    $(".subway_station_add").blur(function(){
-        if($(".subway_station_add").val()!=""){
+    $("#input4").blur(function(){
+        if($("#input2").val()!=""&& $("#input3").val()!=""&& $("#input4").val()!=""){
             $(".station_haved").append("<div class='subway_station_check'>"+
-                $(".subway_station_add").val()+"<span class='glyphicon glyphicon-remove'></span></div>");
+                $("#input2").val()+$("#input3").val()+$("#input4").val()+"<span class='glyphicon glyphicon-remove'></span></div>");
                 del_station();
         }
-        $(this).attr("value","+");
-        $(this).val("+");
     })
 
 })
